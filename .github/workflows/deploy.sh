55ab47148a4d1ce8d40bc035cb44784a2510d3ee
Janine Lu <janinelutw@gmail.com>
2022-01-18 17:41:26 -0800

    gi

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -10,12 +10,18 @@ git config --global user.name "Janine Lu"
 
 git clone git@github.com:janineyylu/janineyylu.github.io.git ${GITHUB_WORKSPACE}/site
 
+echo "got this far.."
+
 rm -rf ${OUTPUT_DIR}
+
 hugo -D 
-# cp -r content/media ${OUTPUT_DIR}/media
+
 cp CNAME ${OUTPUT_DIR}/CNAME
+
 cd $BLOG_GITHUB_CHECKOUT  
+
 cp -r $OUTPUT_DIR/* . 
+
 git status
 git add * 
 git commit -am "new version $NOW "
